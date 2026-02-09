3747d1ef36c2c00c235cbad8ef68e63d75cd8094
cherevon <cherevon1995@gmail.com>
2026-02-09 03:47:01 +0300

    Add minecraft automation apps to the repository

---
+++ app/minecraft/auto_sell/main.cpp
@@ -0,0 +1,98 @@
+﻿// Standard C++ includes
+#include <chrono>
+#include <format>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
+
+// Operating system specific includes
+#include <windows.h>
+
+static constexpr std::chrono::milliseconds KEY_PROCESS_TIME{500};
+
+inline void logInfo(const std::string& message)
+{
+	std::cout << message << std::endl;
+}
+
+inline void logError(const std::string& message)
+{
+	std::cout << std::format("ERROR: {}", message) << std::endl;
+}
+
+INPUT createKeyboardInput(const WORD& vkCode, const DWORD& flags)
+{
+	INPUT result{};
+	result.type = INPUT_KEYBOARD;
+	result.ki.wScan = 0;
+	result.ki.time = 0;
+	result.ki.dwExtraInfo = 0;
+	result.ki.wVk = vkCode;
+	result.ki.dwFlags = flags;
+	return result;
+}
+
+void keyPress(const WORD& vkCode, const std::chrono::milliseconds& handleTime)
+{
+	// Send key press events
+	std::vector<INPUT> inputs{
+		createKeyboardInput(vkCode, 0),
+		createKeyboardInput(vkCode, KEYEVENTF_KEYUP),
+	};
+	SendInput(inputs.size(), inputs.data(), sizeof(INPUT));
+
+	// Let the operating system handle the input
+	std::this_thread::sleep_for(handleTime);
+}
+
+void typeText(const std::string& text, const std::chrono::milliseconds& typeTime)
+{
+	const std::chrono::milliseconds symbTime = typeTime / text.size();
+
+	// Send commands to type the text
+	std::vector<INPUT> inputs{};
+	for (const auto symb : text)
+	{
+		keyPress(VkKeyScan(symb), symbTime);
+	}
+}
+
+void sellItems()
+{
+	using namespace std::chrono_literals;
+
+	// Open chat
+	keyPress(VkKeyScan('t'), 100ms);
+
+	// Type command to sell items
+	typeText("/sell", 250ms);
+
+	// Execute the command by pressing Enter
+	keyPress(VK_RETURN, 100ms);
+}
+
+int main()
+{
+	using namespace std::chrono_literals;
+
+	// Let the user some time to switch to the working window
+	std::this_thread::sleep_for(5s);
+
+	// Constantly sell items
+	int sellCount{0};
+	while (true)
+	{
+		sellItems();
+
+		// Log the number of sell operations executed
+		++sellCount;
+		logInfo(std::format("Items sold {} times", sellCount));
+
+		// Sleep for some time to prevent kicks from the server for too often sell requests
+		std::this_thread::sleep_for(750ms);
+	}
+
+	// Success
+	return 0;
+}
---
+++ app/minecraft/gaia_killer/main.cpp
@@ -0,0 +1,121 @@
+﻿#include <format>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <windows.h>
+
+static constexpr std::chrono::milliseconds KEY_PROCESS_TIME{500};
+
+inline void LogInfo(const std::string& message)
+{
+	std::cout << message << std::endl;
+}
+
+inline void LogError(const std::string& message)
+{
+	std::cout << std::format("ERROR: {}", message) << std::endl;
+}
+
+inline void KeyDown(const HWND winID, const int vkCode)
+{
+	// Press the key
+	SendMessage(winID, WM_KEYDOWN, vkCode, 0);
+
+	// Let the target window handle it
+	std::this_thread::sleep_for(KEY_PROCESS_TIME);
+}
+
+inline void KeyUp(const HWND winID, const int vkCode)
+{
+	// Release the key
+	SendMessage(winID, WM_KEYUP, vkCode, 0);
+
+	// Let the target window handle it
+	std::this_thread::sleep_for(KEY_PROCESS_TIME);
+}
+
+inline void KeyPress(const HWND winID, const int vkCode)
+{
+	// Press and release the key
+	SendMessage(winID, WM_KEYDOWN, vkCode, 0);
+	SendMessage(winID, WM_KEYUP, vkCode, 0);
+
+	// Let the target window handle it
+	std::this_thread::sleep_for(KEY_PROCESS_TIME);
+}
+
+inline void MouseLeftClick(const HWND winID)
+{
+	SendMessage(winID, WM_LBUTTONDOWN, 0, 1);
+	SendMessage(winID, WM_LBUTTONUP, 0, 1);
+}
+
+inline void MouseRightClick(const HWND winID)
+{
+	SendMessage(winID, WM_RBUTTONDOWN, 0, 1);
+	SendMessage(winID, WM_RBUTTONUP, 0, 1);
+}
+
+void MousePressThread(std::stop_token stopToken, const HWND winID)
+{
+	while (!stopToken.stop_requested())
+	{
+		MouseLeftClick(winID);
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+	}
+}
+
+void KillGaia(const HWND winID)
+{
+	static constexpr int GAIA_INGOT_SLOT{4};
+	static constexpr int GAIA_SWORD_SLOT{5};
+
+	static constexpr std::chrono::seconds DAMAGE_DURATION{15};
+
+	static constexpr int VK_0 = 0x30;
+
+	// Select Gaia ingot
+	KeyPress(winID, VK_0 + GAIA_INGOT_SLOT);
+
+	// Use Gaia ingot on the beacon
+	KeyDown(winID, VK_LSHIFT);
+	MouseRightClick(winID);
+	KeyUp(winID, VK_LSHIFT);
+
+	// Select Gaia sword
+	KeyPress(winID, VK_0 + GAIA_SWORD_SLOT);
+
+	// Constantly hit with the sword
+	std::jthread clickerThread(MousePressThread, winID);
+	std::this_thread::sleep_for(DAMAGE_DURATION);
+	clickerThread.request_stop();
+	if (clickerThread.joinable())
+	{
+		clickerThread.join();
+	}
+}
+
+int main()
+{
+	// Find minecraft window
+	LogInfo("Trying to find McSkill OneBlock window...");
+	const auto minecraftWinID = FindWindow(NULL, "McSkill | OneBlock | cherevon");
+	if (!minecraftWinID)
+	{
+		LogError("Failed to find McSkill OneBlock window");
+		return 1;
+	}
+	LogInfo("McSkill OneBlock window found.");
+
+	// Constantly kill Gaia
+	int killCount{0};
+	while (true)
+	{
+		KillGaia(minecraftWinID);
+		++killCount;
+		LogInfo(std::format("Gaia killed {} times", killCount));
+	}
+
+	// Success
+	return 0;
+}
